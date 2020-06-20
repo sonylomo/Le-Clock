a716cdaa42f8cfc954c0fc56371804711ff6cd9b
sonylomo <49971500+sonylomo@users.noreply.github.com>
2020-06-20 21:39:40 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,52 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+//import { render } from 'react-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = this.getTime();
+  }
+  
+  componentDidMount() {
+    this.setTimer();
+  }
+
+  setTimer() {
+    clearTimeout(this.timeout);
+    this.timeout = setTimeout(this.updateClock.bind(this), 1000);
+  }
+
+  updateClock() {
+    this.setState(this.getTime, this.setTimer);
+  }
+
+  getTime() {
+    const currentTime = new Date();
+    return {
+      hrs: currentTime.getHours(),
+      min: currentTime.getMinutes(),
+      sec: currentTime.getSeconds(),
+      ampm: currentTime.getHours() >= 12 ? 'pm' : 'am'
+      
+    }
+  }
+
+  render() {
+    const {hrs, min, sec, ampm} = this.state;
+    return (
+      <div className="App">
+        <h2>My Time.</h2>
+        <div className="Inner">
+        {hrs === 0 ? 12 : hrs > 12 ? hrs - 12 : hrs} :
+        {min > 9 ? min : `0${min}`} : 
+        {sec > 9 ? sec : `0${sec}`} {ampm}
+        </div>
+      </div>
+    );
+  }
 }
+
 
 export default App;
